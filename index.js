736ee04d55427d4c66f8b286d2d6df58649a6ba5
Your Name <you@example.com>
2021-03-25 22:33:21 +0100

    Task 3 and 4 Solved

--- index.js
+++ index.js
@@ -42,7 +42,23 @@ class Airplane {
   */
   
  class Person {
-    
+   constructor (name, age){
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
+   }
+   eat(edible){
+     if (this.stomach.length > 10){
+       this.stomach.push(edible);
+     }
+   }
+  poop (){
+    this.stomach = [];
+  }
+  tooString (){
+    return `${this.name}, ${this.age}`
+  }
+   
   }
   
   /*
@@ -60,7 +76,18 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor (model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+    }
+    fill(gallons) {
+    this.tank = this.tank + items;
+    }
+    drive(gallons) {
+
+    }
   }
   
   /*
@@ -76,7 +103,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attribute){
+      this.name = attribute.name;
+      this.age = attribute.age;
+      this.location = attribute.location;
+    }
+    speak(){ 
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
   }
   
   /*
@@ -93,9 +127,22 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+   constructor(attribute){
+    super(attribute)
+    this.specialty = attribute.specialty;
+    this.favLanguage = attribute.favLanguage;
+    this.catchPhrase = attribute.catchPhrase;
+  }
+  demo(subject){
+    return `Today we are learning about ${subject}`
+  }
+  grade(student, subject){
+    return `${student.name} receives a perfect score on ${subject}`
+  }
+}
 
- }
+ 
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
